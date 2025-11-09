ce3ff7102ce0bb9290594ac22e28c9566018612b
Mahfuz <anonymous.safu@gmail.com>
2025-11-09 21:17:47 +0600

    edit vercel deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HeroCarousel from '../components/Home/Carousel/HeroCarousel';
 import Features from '../components/Home/Features';
 import Product from '../components/Products/Product';
 import DiscountSection from '../components/Home/Discount/DiscountSection';
-import Category from '../components/Home/Categories/category';
+import Category from '../components/Home/Categories/Category';
 
 const Home = () => {
     return (
